446db7ac843da631379f5b22a81b481078bf3948
Turlututur <arthur.bocage@gmail.com>
2022-09-28 13:21:52 +0200

    Envoi de ce qu'on a déjà

---
+++ Screen/SignOutScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text, Button } from 'react-native'
+
+import { TokenContext } from '../Context/Context'
+
+export default function SignOut ({ navigation }) {
+  return (
+    <TokenContext.Consumer>
+      {([token, setToken]) => (
+        <>
+          <Text>Sign Out</Text>
+          <Button title='Sign me out' onPress={() => setToken(null)} />
+        </>
+      )}
+    </TokenContext.Consumer>
+  )
+}
