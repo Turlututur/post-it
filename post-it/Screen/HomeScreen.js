edd6b0ffa7835eeaf38a994fd86acb63c643fa42
Arthur <arthur.bocage@gmail.com>
2023-01-05 10:53:16 +0100

    Message d'accueil en fonction du rôle pour les trois premières connexions et sinon affichage de la liste de projets automatique.

--- post-it/Screen/HomeScreen.js
+++ post-it/Screen/HomeScreen.js
@@ -3,9 +3,16 @@ import { View, Text, Pressable, Image } from "react-native";
 import styles from "../styles/styles";
 import { Link } from "@react-navigation/native";
 
-import { UsernameContext, TokenContext } from "../Context/Context";
+import {
+  UsernameContext,
+  TokenContext,
+  UserRoleContext,
+} from "../Context/Context";
 
 import { getUserData, updateUserNbConnections } from "../API/postItAPI";
+
+import Welcome from "../components/Welcome";
+import ProjectList from "../components/ProjectList";
 
 /**
  * Ecran permettant d'afficher un accueil, base de la navigation de l'app.
@@ -24,105 +31,45 @@ export default function HomeScreen() {
   };
 
   return (
-    <TokenContext.Consumer>
-      {([token, setToken]) => (
-        <UsernameContext.Consumer>
-          {([username, setUsername]) => {
-            getNbConnections(username, token);
-            if (nbConnections < 3) {
-              return (
-                <View style={[styles.container, { marginTop: -50 }]}>
-                  <Image
-                    source={require("../assets/PostItCapt.png")}
-                    style={{ width: 231 / 1.04, height: 140 / 1.04 }}
-                  />
-                  <Image
-                    source={require("../assets/homepage.png")}
-                    style={{ width: 1380 / 4.5, height: 920 / 4.5 }}
-                  />
-                  <Text style={styles.veryTinyText}>
-                    Image by storyset on Freepik
-                  </Text>
-                  <Text style={styles.text}>
-                    {"\n"}Hello {username} et bienvenue chez Post It ! {"\n"}
-                  </Text>
-                  <View style={{ marginLeft: 15, marginRight: 15 }}>
-                    <Text style={styles.tinyText}>
-                      Post It va vous permettre de vous organiser. {"\n"}
-                      En tant que manager, créez des projets et invitez vos
-                      collaborateurs pour qu'ils puissent y déposer leur idées
-                      de post et validez leurs propositions.
-                      {"\n"}
-                      En tant que writter, soumettez vos idées dans les projets
-                      auxquels vous êtes assignés et attendez le feu vert.{" "}
-                      {"\n"}
-                      {"\n"}
-                    </Text>
-                  </View>
-                  <Text style={styles.text}>
-                    {"\n"}3... 2... 1... Postez !{"\n"}
-                  </Text>
-                  <Pressable style={styles.pressable}>
-                    <Text
-                      style={[
-                        styles.tinyTextWhite,
-                        { fontWeight: "bold", fontSize: 17 },
-                      ]}
-                    >
-                      <Link
-                        style={styles.tinyTextWhite}
-                        to={{ screen: "Projets" }}
-                      >
-                        Vos projets
-                      </Link>
-                    </Text>
-                  </Pressable>
-                </View>
-              );
-            } else {
-              return (
-                <View style={[styles.container, { marginTop: -75 }]}>
-                  <Image
-                    source={require("../assets/PostItCapt.png")}
-                    style={{ width: 231 / 1.04, height: 140 / 1.04 }}
-                  />
-                  <Image
-                    source={require("../assets/homepage.png")}
-                    style={{ width: 1380 / 4.5, height: 920 / 4.5 }}
-                  />
-                  <Text style={styles.veryTinyText}>
-                    Image by storyset on Freepik
-                  </Text>
-                  <Text style={styles.text}>
-                    {"\n"}Hello {username} ! {"\n"}
-                  </Text>
-                  <View style={{ marginLeft: 15, marginRight: 15 }}>
-                    <Text style={styles.tinyText}>
-                      Ravis de vous revoir sur Post-It !{"\n"}
-                      {"\n"}
-                    </Text>
-                  </View>
-                  <Pressable style={styles.pressable}>
-                    <Text
-                      style={[
-                        styles.tinyTextWhite,
-                        { fontWeight: "bold", fontSize: 17 },
-                      ]}
-                    >
-                      <Link
-                        style={styles.tinyTextWhite}
-                        to={{ screen: "Projets" }}
-                      >
-                        Vos projets
-                      </Link>
-                    </Text>
-                  </Pressable>
-                </View>
-              );
-            }
-          }}
-        </UsernameContext.Consumer>
+    <UserRoleContext.Consumer>
+      {([userRole, setUserRole]) => (
+        <TokenContext.Consumer>
+          {([token, setToken]) => (
+            <UsernameContext.Consumer>
+              {([username, setUsername]) => {
+                getNbConnections(username, token);
+                if (nbConnections < 3) {
+                  return (
+                    <Welcome
+                      username={username}
+                      token={token}
+                      userRole={userRole}
+                    ></Welcome>
+                  );
+                } else {
+                  return (
+                    <View style={[styles.container, { marginTop: 0 }]}>
+                      <Image
+                        source={require("../assets/managerProject.png")}
+                        style={{ width: 1380 / 4.5, height: 920 / 4.5 }}
+                      />
+                      <Text style={styles.veryTinyText}>
+                        Image by storyset on Freepik
+                      </Text>
+                      {/* Le composant importé : */}
+                      <ProjectList
+                        username={username}
+                        token={token}
+                        userRole={userRole}
+                      ></ProjectList>
+                    </View>
+                  );
+                }
+              }}
+            </UsernameContext.Consumer>
+          )}
+        </TokenContext.Consumer>
       )}
-    </TokenContext.Consumer>
+    </UserRoleContext.Consumer>
   );
 }
---
+++ post-it/components/Welcome.js
@@ -0,0 +1,117 @@
+// npm install react-native-super-grid
+import React from "react";
+import { View, Text, Pressable, Image } from "react-native";
+import { Link } from "@react-navigation/native";
+import styles from "../styles/styles";
+
+/**
+ * Composant permettant d'afficher un message de bienvenue en fonction du rôle
+ * @param {String} username Le nom d'utilisateur connecté
+ * @param {String} token    Le token d'autorisation de l'api
+ * @param {String} userRole Le rôle de l'utilisateur connecté
+ * @returns Un acceuil chaleureux
+ */
+export default function Welcome({ username, token, userRole }) {
+  if (userRole == "manager") {
+    return (
+      <>
+        <View style={[styles.container, { marginTop: -50 }]}>
+          <Image
+            source={require("../assets/PostItCapt.png")}
+            style={{ width: 231 / 1.04, height: 140 / 1.04 }}
+          />
+          <Image
+            source={require("../assets/homepage.png")}
+            style={{ width: 1380 / 4.5, height: 920 / 4.5 }}
+          />
+          <Text style={styles.veryTinyText}>Image by storyset on Freepik</Text>
+          <Text style={styles.text}>
+            {"\n"}Hello {username} et bienvenue chez Post It ! {"\n"}
+          </Text>
+          <View style={{ marginLeft: 15, marginRight: 15 }}>
+            <Text style={styles.tinyText}>
+              Post It va vous permettre de vous organiser. {"\n"}
+              Vous êtes manager, vous pouvez créer des projets. Un projet va
+              pouvoir contenir des brouillons de posts qu'un community manager
+              (writter) pourra créer.
+              {"\n"}En tant que manager, créez des projets, assignez y des
+              community managers et validez (ou non) leurs idées de posts !
+              {"\n"}
+              {"\n"}
+            </Text>
+          </View>
+          <Text style={styles.text}>
+            {"\n"}3... 2... 1... Créez un projet !{"\n"}
+          </Text>
+          <Pressable style={styles.pressable}>
+            <Text
+              style={[
+                styles.tinyTextWhite,
+                { fontWeight: "bold", fontSize: 17 },
+              ]}
+            >
+              <Link style={styles.tinyTextWhite} to={{ screen: "Projets" }}>
+                Vos projets
+              </Link>
+            </Text>
+          </Pressable>
+        </View>
+      </>
+    );
+  }
+
+  if (userRole == "writter") {
+    return (
+      <>
+        <View style={[styles.container, { marginTop: -50 }]}>
+          <Image
+            source={require("../assets/PostItCapt.png")}
+            style={{ width: 231 / 1.04, height: 140 / 1.04 }}
+          />
+          <Image
+            source={require("../assets/homepage.png")}
+            style={{ width: 1380 / 4.5, height: 920 / 4.5 }}
+          />
+          <Text style={styles.veryTinyText}>Image by storyset on Freepik</Text>
+          <Text style={styles.text}>
+            {"\n"}Hello {username} et bienvenue chez Post It ! {"\n"}
+          </Text>
+          <View style={{ marginLeft: 15, marginRight: 15 }}>
+            <Text style={styles.tinyText}>
+              Post It va vous permettre de vous organiser. {"\n"}
+              Vous êtes writter, vous pouvez créer des posts. Un post est un
+              brouillon qui sera validé (ou non) par votre manager, une fois
+              validé, postez le sur les réseaux sociaux.{"\n"}
+              Rendez vous dans un de vos projets pour y créer vos premiers posts
+              à faire valider !{"\n"}
+              Pas de projet ? Attendez qu'un de vos manager vous y assigne.
+              {"\n"}
+            </Text>
+          </View>
+          <Text style={styles.text}>
+            {"\n"}3... 2... 1... Créez un post !{"\n"}
+          </Text>
+          <Pressable style={styles.pressable}>
+            <Text
+              style={[
+                styles.tinyTextWhite,
+                { fontWeight: "bold", fontSize: 17 },
+              ]}
+            >
+              <Link style={styles.tinyTextWhite} to={{ screen: "Projets" }}>
+                Vos projets
+              </Link>
+            </Text>
+          </Pressable>
+        </View>
+      </>
+    );
+  } else {
+    // En théorie ça ne dervait jamais s'afficher
+    return (
+      <>
+        <Text style={styles.text}>Vous n'avez pas de rôle ???</Text>
+      </>
+    );
+  }
+}
